5cc1adb54d8a0880cfd53bc8f85511980cca5978
WaltGurley <walt.gurley@gmail.com>
2019-08-23 16:35:30 -0400

    initial demo

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <header>
+      <h1>NOAA-16</h1>
+      <h2>Weather Satellite Data</h2>
+    </header>
+    <main class="video-container">
+      <VideoPanel
+        v-for="n in testNum"
+        videoSrc="http://52.54.73.208/temp.mp4"
+        :videoId="n"
+        :key="n"
+      />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import VideoPanel from "./components/VideoPanel.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    VideoPanel
+  },
+  data () {
+    return {
+      testNum: [1, 2, 3]
+    }
+  },
+  methods: {
+
   }
 };
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +42,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.video-container {
+  display: flex;
+  align-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/VideoPanel.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="video-panel">
+    <h3>Source {{ videoId }}</h3>
+    <video class="video" autoplay muted="true" controls>
+      <source :src="videoSrc" type="video/mp4">
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoPanel",
+  props: {
+    videoSrc: String,
+    videoId: Number
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@media screen and (max-width: 425px) {
+  .video-panel {
+    width: 98%;
+  }
+
+  .video {
+    width: 100%;
+  }
+}
+</style>
